2c294ba07510f23702d256b81f9c3f7fb9f35b8e
Mukesh Otwani <mukeshotwani.50@gmail.com>
2024-09-14 09:31:22 +0530

    sample test added

---
+++ src/test/java/tests/SampleTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class SampleTest 
+{
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("Sample Test By RestAssured");
+	}
+
+}
